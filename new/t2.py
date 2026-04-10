fc291656080a7cf6b102e93acccbf01e7f8da8d1
Gabriel A Weiderpass <ga.weiderpass@gmail.com>
2026-04-09 23:54:39 -0500

    change

--- new/t2.py
+++ new/t2.py
@@ -1,3 +1,7 @@
 print('Hello, World!')
 print('Hello, World!2')
 print('Hello, World!3')
+print('Hello, World!4')
+print('stop this non' + 'sense')
+print('stop this non' + 'sense' + '!')
+print('stop this non' + 'sense' + '!' + '!')
